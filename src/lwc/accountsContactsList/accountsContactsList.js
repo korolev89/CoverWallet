d6de99fb37f381a62d0d1b7d58f84fbae8ca8ccc
anton_korolev@epam.com <anton_korolev@epam.com>
2022-05-20 13:22:38 +0300

    changed multiple arrays to isVisible property, implemented contact cards

--- src/lwc/accountsContactsList/accountsContactsList.js
+++ src/lwc/accountsContactsList/accountsContactsList.js
@@ -1,18 +1,19 @@
-import {LightningElement} from 'lwc';
+import {LightningElement, track} from 'lwc';
 import getAllAccountsContacts from '@salesforce/apex/AccountsContactsController.getAllAccountsContacts';
 
 export default class AccountsContactsList extends LightningElement {
-    accounts;
-    filteredAccounts;
-    error;
+    // track is using to update isVisible parameter and "refresh" records on the screen
+    @track accounts = [];
+    @track contacts = [];
+    error = [];
 
-    accountsCount = 0;
+    accountsTotalCount = 0;
     recordsPerPage = 3;
     currentPage = 1;
-    numberOfPages = 0;
+    totalPages = 1;
     trimStart = 0;
     trimEnd = 0;
-    paginationPages = [];
+    pagesArray = [];
 
     connectedCallback() {
         this.loadAccountsContact();
@@ -21,9 +22,24 @@ export default class AccountsContactsList extends LightningElement {
     loadAccountsContact() {
         getAllAccountsContacts()
             .then(result => {
+                console.log('result = ', result);
                 this.accounts = result;
-                this.filteredAccounts = result;
-                this.accountsCount = result.length;
+                const visibleContacts = [];
+                this.accounts.forEach(function(account) {
+                    console.log('account = ', account);
+                    account.isVisible = true;
+                    const contactsList = account.Contacts;
+                    if (contactsList) {
+                        console.log('contacts = ', contactsList);
+                        contactsList.forEach(function(contact) {
+                            contact.isVisible = true;
+                            // error here
+                            visibleContacts.push(contact);
+                        })
+                    }
+                });
+                this.contacts = visibleContacts;
+                this.accountsTotalCount = result.length;
                 this.buildPagination(1);
             })
             .catch(error => {
@@ -32,12 +48,20 @@ export default class AccountsContactsList extends LightningElement {
     }
 
     applyFilter(event) {
-        this.filteredAccounts = this.accounts.filter(account => (account.Name.toLowerCase().includes(event.target.value)));
+        this.accounts.forEach(function(account) {
+            if (account.Name.toLowerCase().includes(event.target.value.toLowerCase())) {
+                account.isVisible = true;
+            } else {
+                account.isVisible = false;
+            }
+        });
     }
 
-    changePagination(event) {
+    changeRecordsPerPageCount(event) {
         this.recordsPerPage = event.target.value;
-        this.buildPagination(this.currentPage);
+        if (this.recordsPerPage != 0) {
+            this.buildPagination(this.currentPage);
+        }
     }
 
     changePage(event) {
@@ -51,18 +75,38 @@ export default class AccountsContactsList extends LightningElement {
         this.trimStart = (currentPage - 1) * parseInt(this.recordsPerPage);
         this.trimEnd = this.trimStart + parseInt(this.recordsPerPage);
 
-        // this.filteredAccounts = this.accounts;
-        this.filteredAccounts = this.accounts.slice(this.trimStart, this.trimEnd);
+        const tStart = this.trimStart;
+        const tEnd = this.trimEnd;
 
-        this.numberOfPages = Math.ceil(this.accountsCount / this.recordsPerPage);
-        for (let index = 1; index <= this.numberOfPages; index++) {
-            this.paginationPages.push(index);
+        this.accounts.forEach(function(account, i) {
+            if (i >= tStart && i < tEnd) {
+                account.isVisible = true;
+            } else {
+                account.isVisible = false;
+            }
+        });
+
+        this.totalPages = Math.ceil(this.accountsTotalCount / this.recordsPerPage);
+        for (let index = 1; index <= this.totalPages; index++) {
+            this.pagesArray.push(index);
         }
     }
 
     eraseValues() {
-        this.paginationPages = [];
+        this.pagesArray = [];
         this.trimStart = 0;
         this.trimEnd = 0;
+        this.currentPage = 1;
+    }
+
+    displayContacts(event) {
+        console.log(event.currentTarget.id);
+        this.contacts.forEach(function (contact) {
+            if (contact.AccountId == event.currentTarget.id.substring(0, 18)) {
+                contact.isVisible = true;
+            } else {
+                contact.isVisible = false;
+            }
+        });
     }
 }
